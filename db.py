9aaec66e3dbd75ee94b5c1cfd4b6b84aa277c7d0
leeban99 <leeban99@gmail.com>
2021-06-19 10:18:47 +0530

    python for sqlite

--- db.py
+++ db.py
@@ -3,3 +3,12 @@ import sqlite3
 conn = sqlite3.connect('test.db')
 
 print("Opened database successfully")
+
+conn.execute('''CREATE TABLE ZSCALER_VERSIONS
+         (ID INT PRIMARY KEY     NOT NULL,
+         VERSION TEXT NOT NULL,
+         REMARKS TEXT);''')
+
+print("Table created successfully")
+
+conn.close()
